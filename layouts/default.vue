1da5daa4dace3852329e67d89aed254265794dc7
Kevin <aexck8836660@gmail.com>
2022-10-28 15:03:39 +0800

    first_commit

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <Nuxt class="main" />
+    <Footer></Footer>
+    <!-- <Header class="sticky top-0 z-10"></Header>
+    -->
+  </div>
+</template>
+<style lang="scss" scoped>
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+.main {
+  flex: 1;
+}
+// #app {
+//   display: flex;
+//   flex-direction: column;
+//   min-height: 100vh;
+// }
+// // // .frontend {
+// // //   min-height: 100vh;
+// // // }
+// .main {
+//   flex: 1;
+// }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="text-red-500 font-bold">123</div>
+</template>
+
+<script>
+export default {};
+</script>
